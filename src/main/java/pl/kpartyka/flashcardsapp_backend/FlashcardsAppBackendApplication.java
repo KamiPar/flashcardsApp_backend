f1bce53cc8062e2c8cd9bb7d902e4f02861dd2ca
Kamil <Partyka.K98@gmail.com>
2023-11-12 13:58:14 +0100

    Initial Commit

---
+++ src/main/java/pl/kpartyka/flashcardsapp_backend/FlashcardsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.kpartyka.flashcardsapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardsAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kpartyka/flashcardsapp_backend/FlashcardsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kpartyka.flashcardsapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
